19467171d1ba0dd01c2456198afca8c2e3c9f0e6
atsushi0124 <h19170004@gmail.com>
2024-02-21 21:10:51 +0900

    firstcommit

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import {Inter} from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import {Footer} from "../components/Footer";
+import {Footer} from "@/components/Footer";
 
-// const inter = Inter({subsets: ["latin"]});
+const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
   return (
@@ -59,6 +59,7 @@ export default function Home() {
             />
           </div>
         </div>
+
         <Footer />
       </main>
     </>
