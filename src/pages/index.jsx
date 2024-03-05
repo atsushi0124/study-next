f09788d47d6a31a424781ecb18037c58f9a1fe97
atsushi0124 <h19170004@gmail.com>
2024-03-05 22:55:31 +0900

    テキスト情報を配列に追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,10 +9,11 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClickPlus = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
@@ -32,18 +33,36 @@ export default function Home() {
   }, []);
 
   const handleDesplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素がすでに存在します");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   return (
     <>
       <Headline title="Index Page" />
-      <div className={styles.btnArea}>
-        {isShow ? <h1 className={styles.title}>{count}</h1> : null}
-        <button onClick={handleDesplay}>{isShow ? "非表示" : "表示"}</button>
-        <button className={styles.btn} onClick={handleClickPlus}>
-          クリック
-        </button>
+      <div className={styles.container}>
+        <div className={styles.btnArea}>
+          {isShow ? <h1 className={styles.title}>{count}</h1> : null}
+          <button onClick={handleDesplay}>{isShow ? "非表示" : "表示"}</button>
+          <button className={styles.btn} onClick={handleClickPlus}>
+            クリック
+          </button>
+        </div>
+        <ul className={styles.arrayList}>
+          {array.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+        <button onClick={handleAdd}>追加</button>
       </div>
       <input type="text" value={text} onChange={handleClickText} />
       <main className={styles.main}>
