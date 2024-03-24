6420ed6b3052fbfc27cf2942071f0a83d26bcd2a
atsushi0124 <h19170004@gmail.com>
2024-03-24 18:41:43 +0900

    useStateの検証

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -2,44 +2,59 @@ import styles from "@/styles/Home.module.css";
 import {useCallback, useState} from "react";
 
 export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  //   const [posts, setPosts] = useState([]);
+  //   const [loading, setLoading] = useState(true);
+  //   const [error, setError] = useState(null);
+
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+    error: null,
+  });
 
   const getPosts = useCallback(async () => {
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      console.log(res);
+      //   console.log(res);
       if (!res.ok) {
         throw new Error("データの取得に失敗しました");
       }
       const json = await res.json();
-      setPosts(json);
+      //   setState.data(json);
+      setState((prevState) => {
+        return {
+          data: json,
+          loading: false,
+          error: null,
+        };
+      });
     } catch (error) {
-      setError(error);
+      //   setState.error(error);
     }
-    setLoading(false);
+    // state.loading(false);
   }, []);
 
   useState(() => {
     getPosts();
   }, [getPosts]);
 
+  console.log("foo");
+
   //   api先のデータを取得中
-  if (loading) {
+  if (state.loading) {
     return <div>ローディング中です</div>;
   }
 
   //   apiのデータを取得できなかった時
-  if (error) {
+  if (state.error) {
     return <div>{error.message}</div>;
   }
 
   //   api先のデータを取得できたとき
-  if (posts.length > 0) {
+  if (state.data.length > 0) {
     return (
       <ol className={styles.ol}>
-        {posts.map((post) => {
+        {state.data.map((post) => {
           return (
             <li key={post.id} className={styles.container}>
               {post.title}
